163729c8055c028f2253c6c23762f8397fd42d8b
Irina Balanel <irina.cowork@gmail.com>
2024-08-16 22:26:54 -0400

    Updated

--- portfolio-front/src/App.jsx
+++ portfolio-front/src/App.jsx
@@ -4,8 +4,6 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
 
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
